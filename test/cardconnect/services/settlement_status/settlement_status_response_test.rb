8167900434c1b5eafdebc2d39e014a2729bf25d4
thetizzo <j.m.taylor1@gmail.com>
2015-02-05 12:12:19 -0700

    Tests for SettlementStatusResponse

--- test/cardconnect/services/settlement_status/settlement_status_response_test.rb
+++ test/cardconnect/services/settlement_status/settlement_status_response_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 describe SettlementStatusResponse do
   before do
     @response = SettlementStatusResponse.new(valid_settlestat_response)
-    @valid_payload = valid_settlestat_response.first
+    @valid_payload = symbolize_keys(valid_settlestat_response.first)
+    @valid_payload[:txns] = @valid_payload[:txns].map{|txn| symbolize_keys(txn)}
   end
 
   after do
@@ -12,34 +13,40 @@ describe SettlementStatusResponse do
   end
 
   it 'should have the merchant id' do
-    @response.merchid.must_equal @valid_payload['merchid']
+    @response.merchid.must_equal @valid_payload[:merchid]
   end
 
   it 'should have the batch id' do
-    @response.batchid.must_equal @valid_payload['batchid']
+    @response.batchid.must_equal @valid_payload[:batchid]
   end
 
   it 'should have the settlement status' do
-    @response.hoststat.must_equal @valid_payload['hoststat']
+    @response.hoststat.must_equal @valid_payload[:hoststat]
   end
 
   it 'should have the host batch id' do
-    @response.hostbatch.must_equal @valid_payload['hostbatch']
+    @response.hostbatch.must_equal @valid_payload[:hostbatch]
   end
 
   it 'should have the clearing house' do
-    @response.respproc.must_equal @valid_payload['respproc']
+    @response.respproc.must_equal @valid_payload[:respproc]
   end
 
   it 'should have the transactions' do
     @response.txns.each_with_index do |txn, i|
-      txn.must_equal symbolize_keys(@valid_payload['txns'][i])
+      txn.must_equal @valid_payload[:txns][i]
     end
   end
 
   describe '#body' do
     it 'should have all the right fields in the body' do
       @response.body.keys.must_equal [:merchid, :batchid, :hoststat, :hostbatch, :respproc, :txns]
+    end
+
+    it 'should generate hash with all the right values' do
+      @response.body.keys.each do |k|
+        @response.body[k].must_equal @valid_payload[k]
+      end
     end
   end
 end
