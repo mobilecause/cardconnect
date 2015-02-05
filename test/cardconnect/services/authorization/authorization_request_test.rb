d7ed99787b2a5cbdaadfe3100cde86466ad073ac
thetizzo <j.m.taylor1@gmail.com>
2015-02-05 13:52:02 -0700

    Tests for Authorization request and response

--- test/cardconnect/services/authorization/authorization_request_test.rb
+++ test/cardconnect/services/authorization/authorization_request_test.rb
@@ -4,16 +4,104 @@ describe AuthorizationRequest do
   describe "#payload" do
     before do
       @request = AuthorizationRequest.new(valid_auth_request)
+      @valid_payload = symbolize_keys(valid_auth_request)
     end
 
     after do
       @request = nil
     end
 
-    it 'should generate hash with all the right values' do
-      valid_payload = symbolize_keys(valid_auth_request)
-      @request.payload.keys.each do |k|
-        @request.payload[k].must_equal valid_payload[k]
+    describe 'FIELDS' do
+      it 'should have merchant id' do
+        @request.merchid.must_equal @valid_payload[:merchid]
+      end
+
+      it 'should have account' do
+        @request.account.must_equal @valid_payload[:account]
+      end
+
+      it 'should have expiry' do
+        @request.expiry.must_equal @valid_payload[:expiry]
+      end
+
+      it 'should have amount' do
+        @request.amount.must_equal @valid_payload[:amount]
+      end
+
+      it 'should have currency' do
+        @request.currency.must_equal @valid_payload[:currency]
+      end
+
+      it 'should have account type' do
+        @request.accttype.must_equal @valid_payload[:accttype]
+      end
+
+      it 'should have name' do
+        @request.name.must_equal @valid_payload[:name]
+      end
+
+      it 'should have address' do
+        @request.address.must_equal @valid_payload[:address]
+      end
+
+      it 'should have city' do
+        @request.city.must_equal @valid_payload[:city]
+      end
+
+      it 'should have country' do
+        @request.country.must_equal @valid_payload[:country]
+      end
+
+      it 'should have phone' do
+        @request.phone.must_equal @valid_payload[:phone]
+      end
+
+      it 'should have postal' do
+        @request.postal.must_equal @valid_payload[:postal]
+      end
+
+      it 'should have email' do
+        @request.email.must_equal @valid_payload[:email]
+      end
+
+      it 'should have ecomind' do
+        @request.ecomind.must_equal @valid_payload[:ecomind]
+      end
+
+      it 'should have cvv2' do
+        @request.cvv2.must_equal @valid_payload[:cvv2]
+      end
+
+      it 'should have order id' do
+        @request.orderid.must_equal @valid_payload[:orderid]
+      end
+
+      it 'should have track' do
+        @request.track.must_equal @valid_payload[:track]
+      end
+
+      it 'should have bankaba' do
+        @request.bankaba.must_equal @valid_payload[:bankaba]
+      end
+
+      it 'should have tokenize' do
+        @request.tokenize.must_equal @valid_payload[:tokenize]
+      end
+
+      it 'should have termid' do
+        @request.termid.must_equal @valid_payload[:termid]
+      end
+
+      it 'should have capture' do
+        @request.capture.must_equal @valid_payload[:capture]
+      end
+    end
+
+    describe '#payload' do
+      it 'should generate hash with all the right values' do
+        @request.payload.keys.each do |k|
+          @request.payload[k].must_equal @valid_payload[k]
+        end
       end
     end
   end
--- test/cardconnect/services/authorization/authorization_response_test.rb
+++ test/cardconnect/services/authorization/authorization_response_test.rb
@@ -11,59 +11,58 @@ describe AuthorizationResponse do
     @valid_payload = nil
   end
 
-  #:respstat, :retref, :account, :token, :amount, :respcode,
-  #              :resptext, :respproc, :avsresp, :cvvresp, :authcode, :commcard
+  describe 'FIELDS' do
+    it 'should have the merchant id' do
+      @response.merchid.must_equal @valid_payload[:merchid]
+    end
 
-  it 'should have the merchant id' do
-    @response.merchid.must_equal @valid_payload[:merchid]
-  end
+    it 'should have the status' do
+      @response.respstat.must_equal @valid_payload[:respstat]
+    end
 
-  it 'should have the status' do
-    @response.respstat.must_equal @valid_payload[:respstat]
-  end
+    it 'should have the Retrieval Reference Number' do
+      @response.retref.must_equal @valid_payload[:retref]
+    end
 
-  it 'should have the Retrieval Reference Number' do
-    @response.retref.must_equal @valid_payload[:retref]
-  end
+    it 'should have the Account Number' do
+      @response.account.must_equal @valid_payload[:account]
+    end
 
-  it 'should have the Account Number' do
-    @response.account.must_equal @valid_payload[:account]
-  end
+    it 'should have the Token' do
+      @response.token.must_equal @valid_payload[:token]
+    end
 
-  it 'should have the Token' do
-    @response.token.must_equal @valid_payload[:token]
-  end
+    it 'should have the Amount' do
+      @response.amount.must_equal @valid_payload[:amount]
+    end
 
-  it 'should have the Amount' do
-    @response.amount.must_equal @valid_payload[:amount]
-  end
+    it 'should have the Response Code' do
+      @response.respcode.must_equal @valid_payload[:respcode]
+    end
 
-  it 'should have the Response Code' do
-    @response.respcode.must_equal @valid_payload[:respcode]
-  end
+    it 'should have the Response text' do
+      @response.resptext.must_equal @valid_payload[:resptext]
+    end
 
-  it 'should have the Response text' do
-    @response.resptext.must_equal @valid_payload[:resptext]
-  end
+    it 'should have the Response Processor' do
+      @response.respproc.must_equal @valid_payload[:respproc]
+    end
 
-  it 'should have the Response Processor' do
-    @response.respproc.must_equal @valid_payload[:respproc]
-  end
+    it 'should have the AVS response code' do
+      @response.avsresp.must_equal @valid_payload[:avsresp]
+    end
 
-  it 'should have the AVS response code' do
-    @response.avsresp.must_equal @valid_payload[:avsresp]
-  end
+    it 'should have the CVV response code' do
+      @response.cvvresp.must_equal @valid_payload[:cvvresp]
+    end
 
-  it 'should have the CVV response code' do
-    @response.cvvresp.must_equal @valid_payload[:cvvresp]
-  end
+    it 'should have the Authorization code' do
+      @response.authcode.must_equal @valid_payload[:authcode]
+    end
 
-  it 'should have the Authorization code' do
-    @response.authcode.must_equal @valid_payload[:authcode]
-  end
-
-  it 'should have the Commercial Card Flag' do
-    @response.commcard.must_equal @valid_payload[:commcard]
+    it 'should have the Commercial Card Flag' do
+      @response.commcard.must_equal @valid_payload[:commcard]
+    end
   end
 
   describe "#body" do
