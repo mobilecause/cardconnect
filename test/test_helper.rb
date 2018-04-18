3629df90c9fdec784031c7237746b36b3ec70a8b
Shivashankar P <shivashankar.ror@gmail.com>
2018-04-18 08:43:14 +0530

    Removed duplicate fields in Inquire response

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,6 +18,8 @@ end
 # Stub out the Faraday connection for testing.
 module CardConnect
   class Connection
+    undef :connection if method_defined? :connection
+
     def connection
       @connection ||= Faraday.new(faraday_options) do |faraday|
         faraday.request :basic_auth, @config.api_username, @config.api_password
