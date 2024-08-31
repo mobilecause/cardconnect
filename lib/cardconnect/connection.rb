b58a70d6b86a2101ec579a1f5c8c33fde23cfac1
Jorge Caballero <jacaballero93@gmail.com>
2024-08-30 20:57:31 -0500

    Version 2.5.0 Using manual auth definition

--- lib/cardconnect/connection.rb
+++ lib/cardconnect/connection.rb
@@ -8,7 +8,10 @@ module CardConnect
 
     def connection
       @connection ||= Faraday.new(faraday_options) do |f|
-        f.request :basic_auth, @config.api_username, @config.api_password
+        # f.request :basic_auth, @config.api_username, @config.api_password
+
+        credentials = Base64.strict_encode64("#{@config.api_username}:#{@config.api_password}")
+        f.headers['Authorization'] = "Basic #{credentials}"
         f.request :json
 
         f.response :json, content_type: /\bjson$/
